624d215d8ad5d388251c5e62c840f456d68fb8ec
Jack Kellenberger <jackson@kellenberger.io>
2023-10-17 20:35:14 -0400

    Track scores

--- js/game.js
+++ js/game.js
@@ -70,6 +70,7 @@ function tick() {
     }
 
     if (player.dead) {
+        addScore(score, false);
         gameState = "dead";
     }
 
@@ -82,13 +83,44 @@ function tick() {
     }
 }
 
+function drawScores() {
+    let scores = getScores();
+    if (scores.length) {
+        drawText(
+            rightPad(["RUN", "SCORE", "TOTAL"]),
+            18,
+            true,
+            canvas.height / 2,
+            "white"
+        );
+        let newestScore = scores.pop();
+        scores.sort(function (a, b) {
+            return b.totalScore - a.totalScore;
+        });
+        scores.unshift(newestScore);
+
+        for (let i = 0; i < Math.min(10, scores.length); i++) {
+            let scoreText = rightPad([scores[i].run, scores[i].score, scores[i].totalScore]);
+            drawText(
+                scoreText,
+                18,
+                true,
+                canvas.height / 2 + 24 + i * 24,
+                i == 0 ? "aqua" : "violet"
+            )
+        }
+    }
+}
+
+
 function showTitle() {
     ctx.fillStyle = 'rgba(0,0,0,.75)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     gameState = "title";
 
     drawText("DIM SPIRITS", 40, true, canvas.height / 2 - 110, "white");
-    drawText("PRESS ANY KEY TO CONTINUE", 25, true, canvas.height / 2, "white");
+    drawText("PRESS ANY KEY TO CONTINUE", 25, true, canvas.height / 2 - 50, "white");
+    drawScores();
 }
 
 function startGame() {
@@ -106,4 +138,28 @@ function startLevel(playerHp) {
     player.hp = playerHp
 
     randomPassableTile().replace(Exit);
+}
+
+function getScores() {
+    if (localStorage["scores"]) {
+        return JSON.parse(localStorage["scores"]);
+    } else {
+        return [];
+    }
+}
+
+function addScore(score, won) {
+    let scores = getScores();
+    let scoreObject = { score: score, run: 1, totalScore: score, active: won };
+    let lastScore = scores.pop();
+    if (lastScore) {
+        if (lastScore.active) {
+            scoreObject.run = lastScore.run + 1;
+            scoreObject.totalScore += lastScore.totalScore;
+        } else {
+            scores.push(lastScore);
+        }
+    }
+    scores.push(scoreObject);
+    localStorage["scores"] = JSON.stringify(scores);
 }
